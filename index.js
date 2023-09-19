548660d4284dc29720ee06d1abefbfa2b13bea92
Isaac Song <isaacsongs10@gmail.com>
2023-09-19 09:54:22 -0700

    Add JS Fundamentals Quiz

---
+++ index.js
@@ -0,0 +1,17 @@
+//! Quiz Review
+//TODO: JavaScript Fundamentals Quiz
+/* 
+    Strict inequality operator: !==
+    Strict equality operator: ===
+
+    Logical Operators:
+        "&&": AND
+        "||": OR
+        "!": NOT
+
+    Variable Declarators
+        const: Cannot be re-assigned or re-declared
+        let: Can be re-assigned, not re-declared (in same scope)
+        var: Can be re-assigned and re-declared in the same scope (Can lead to scope-related bugs)
+*/
+
